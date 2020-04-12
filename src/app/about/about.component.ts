cd635c47868d3aaab08af623a0b5dbbbf331a2c8
Arya Vatan-Abadi <arya.vatanabadi@gmail.com>
2020-04-11 20:41:28 -0400

    Created Data.json
    Data.json file in Assets folder now holds list of programming skills. The Home and About page now read from this file to display the programming skills. This means that in the future if I want to add a new skill, I only need to add it to the Data.json file and it will update everywhere.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import data from '../../assets/data.json';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  skills: Object;
+
   constructor() { }
 
   ngOnInit() {
+    this.skills = data.skills;
+    console.log(this.skills);
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import data from '../../assets/data.json';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  skills: Object;
+
   constructor() { }
 
   ngOnInit() {
+    this.skills = data.skills;
+    console.log(this.skills);
   }
 
 }
