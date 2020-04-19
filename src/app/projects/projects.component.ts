6258d5186cd51a8f9995bf83db775a7bfdba07d3
Arya Vatan-Abadi <arya.vatanabadi@gmail.com>
2020-04-19 19:25:19 -0400

    Project filtering
    Projects can be filtered now by the technologies used to build them (i.e., JavaScript, HTML, etc.)

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -4,24 +4,77 @@ import data from "../../assets/Data.json";
 import { ProjectService } from '../project.service';
 
 @Component({
-  selector: 'app-projects',
-  templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+	selector: 'app-projects',
+	templateUrl: './projects.component.html',
+	styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
 
-  projects: Object;
+	allProjects: any;
+	projects: any;
+	filters: any;
+	selectedFilter: any;
 
-  constructor(private router: Router, private projectService : ProjectService) { }
+	constructor(private router: Router, private projectService: ProjectService) { }
 
-  ngOnInit() {
-    this.projects = data.projects;
-    console.log(this.projects);
-  }
+	ngOnInit() {
+		this.allProjects = data.projects;
+		this.projects = JSON.parse(JSON.stringify(this.allProjects)); // Make deep copy
 
-  projectClicked(project){
-    this.projectService.setSelectedProject(project);
-    this.router.navigate(['projects/project-details']);
-  }
+		this.getFilterSkills();  // Extract Skills from the projects
+	}
+
+	projectClicked(project) {
+		this.projectService.setSelectedProject(project);
+		this.router.navigate(['projects/project-details']);
+	}
+
+	getFilterSkills() {
+		let list = [];
+		this.projects.forEach(project => {
+			project.skills.forEach(skill => {
+				let duplicate = false;
+
+				list.forEach(filter => {
+					if(filter.name == skill){
+						duplicate = true;
+					}
+				})
+
+				if (!duplicate) {
+					list.push({name:skill, class:""});
+				}
+			})
+		});
+		this.selectedFilter = 'selected';  // This will initially select "Show All" option
+		this.filters = list.sort();
+	}
+
+	filterSelected(filter){		
+		// First unselect previous filter
+		if(this.selectedFilter != undefined && this.selectedFilter != 'selected'){
+			this.selectedFilter.class = "";
+		}
+		
+		// If "Show All" is clicked, reload all projects, change selectedFilter so "Show All" button is active
+		if(filter.name == 'Show All'){
+			this.selectedFilter = 'selected'
+			this.projects = JSON.parse(JSON.stringify(this.allProjects));
+			return;
+		}
+
+		// Select the filter so the button looks selected
+		filter.class = "selected";
+		this.selectedFilter = filter;
+
+		// Get projects that match filter from master list
+		let filteredProjects  = [];
+		this.allProjects.forEach(project => {
+			if(project.skills.includes(filter.name)){
+				filteredProjects.push(project);
+			}
+		});
+		this.projects = filteredProjects;  // Assign to data-binded projects variable
+	}
 
 }
