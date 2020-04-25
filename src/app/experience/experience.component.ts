d480d59753e00bcecb1293008826ff07c8cf2679
Arya Vatan-Abadi <arya.vatanabadi@gmail.com>
2020-04-25 13:21:02 -0400

    Experiences Refactored to Data.JSON

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import data  from "../../assets/Data.json";
 
 @Component({
-  selector: 'app-experience',
-  templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.scss']
+	selector: 'app-experience',
+	templateUrl: './experience.component.html',
+	styleUrls: ['./experience.component.scss']
 })
 export class ExperienceComponent implements OnInit {
 
-  constructor() { }
+	experiences: any;
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+		this.experiences = data.experiences;
+	}
 
 }
